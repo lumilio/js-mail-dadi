9b781a0722cb2af3d1eaa22f95a845ce11b21d31
lumilio <logistic-division@lumilium.com>
2021-10-14 16:37:31 +0200

    aggiunta commenti

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,6 +1,14 @@
 
+// inizializzo variabili
+
 const userEmail = document.getElementById('campo_email');
 const tryButton = document.getElementById('generate');
+let messaggio1 = document.getElementById('messaggio1')
+let manoPlayer = document.getElementById('dado-utente')
+let manoComputer = document.getElementById('dado-computer')
+let messaggio2 = document.getElementById('messaggio2')
+
+// creo un arrey che contenga le gli input per accedere al gioco
 
 const emailList = [
     'mario.rossi@gmail.com',
@@ -9,21 +17,19 @@ const emailList = [
     'mario.neri@gmail.com',
 ]
 
-let messaggio1 = document.getElementById('messaggio1')
-let manoPlayer = document.getElementById('dado-utente')
-let manoComputer = document.getElementById('dado-computer')
-let messaggio2 = document.getElementById('messaggio2')
+// creo due variabili che emulino il dado a 6 facce
 
 const dadoPlayer = Math.floor(Math.random() * 6) + 1
 const dadoComputer = Math.floor(Math.random() * 6) + 1
 
-
+// rendo interattivo il pulsante di verifica
 
 tryButton.addEventListener('click', function () {
 
     const tryEmail = userEmail.value;
     console.log(tryEmail);
 
+    // se l'utente non inserire una corretta opzione di accesso non giocherà
 
     for (let i = 0; i < tryEmail.length; i++) {
 
@@ -31,6 +37,9 @@ tryButton.addEventListener('click', function () {
             messaggio1.textContent = 'login effettuato correttamente, giochiamo a dadi!'
             manoPlayer.textContent = `il tuo dado ha fatto ${dadoPlayer}`
             manoComputer.textContent = `il mio dado ha fatto ${dadoComputer}`
+
+            // aggiungo un verdetto in caso dei 3 risultati possibili
+
             if (dadoPlayer < dadoComputer){
                 messaggio2.textContent = 'hai perso!' 
             }
