80c5e78f4153c1bb379cb11c2279953ced4ad211
lumilio <logistic-division@lumilium.com>
2021-10-14 14:29:30 +0200

    validazione email

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,34 +1,41 @@
 
-const theList = document.querySelector('#list');
 
-// genero 100 li con un loop for all'iterno di ul 
 
-for (let i = 1; i <= 100; i++ ) {
-    const theBox = document.createElement("li");
-    theBox.append(i)
-    theList.append(theBox)
 
-// con le istruzioni condizionali cambio il colore dei box multipli di 3 e 5 
 
-    if (i % 3 == 0 && i % 5 == 0) {
-        theBox.innerHTML = "fizzbuzz"
-        theBox.style.backgroundColor = "#DE5471";
+
+
+//Chiedi all’utente la sua email, 
+
+const userEmail = prompt('inserisci la tua mail');
+const emailList = [
+    'mario.rossi@gmail.com',
+    'mario.bianchi@gmail.com',
+    'mario.verdi@gmail.com',
+    'mario.neri@gmail.com',
+]
+
+for (var i = 0; i < userEmail.length; i++) {
+    if (userEmail == emailList[i]) {
+      alert('login effettuato correttamente!');
+      break;
+    } else {
+      alert('login errato!')
     }
-    else if (i % 3 == 0) {
-        theBox.innerHTML = "fizz"
-        theBox.style.backgroundColor = "#61D2A4";
-    }
-    else if (i % 5 == 0) {
-        theBox.innerHTML = "buzz"
-        theBox.style.backgroundColor = "#F9D277";
-    }
-    else {
-        theBox.style.backgroundColor = "#3D88AE";
-    }
-} 
+  }
 
 
 
+
+
+
+
+
+
+
+
+// stampa un messaggio appropriato sull’esito del controllo.
+alert
 
 
 
