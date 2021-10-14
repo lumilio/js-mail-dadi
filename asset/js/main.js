99d6b542a17a61062bf9ad7ccb23d8a9bb80eb89
lumilio <logistic-division@lumilium.com>
2021-10-14 16:31:32 +0200

    aggiunta messaggi del risultato + dadi

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,21 @@
 
 const userEmail = document.getElementById('campo_email');
 const tryButton = document.getElementById('generate');
+
 const emailList = [
     'mario.rossi@gmail.com',
     'mario.bianchi@gmail.com',
     'mario.verdi@gmail.com',
     'mario.neri@gmail.com',
 ]
+
 let messaggio1 = document.getElementById('messaggio1')
+let manoPlayer = document.getElementById('dado-utente')
+let manoComputer = document.getElementById('dado-computer')
+let messaggio2 = document.getElementById('messaggio2')
+
+const dadoPlayer = Math.floor(Math.random() * 6) + 1
+const dadoComputer = Math.floor(Math.random() * 6) + 1
 
 
 
@@ -21,9 +29,19 @@ tryButton.addEventListener('click', function () {
 
         if (tryEmail == emailList[i]) {
             messaggio1.textContent = 'login effettuato correttamente, giochiamo a dadi!'
+            manoPlayer.textContent = `il tuo dado ha fatto ${dadoPlayer}`
+            manoComputer.textContent = `il mio dado ha fatto ${dadoComputer}`
+            if (dadoPlayer < dadoComputer){
+                messaggio2.textContent = 'hai perso!' 
+            }
+            else if (dadoPlayer > dadoComputer){
+                messaggio2.textContent = 'hai vinto!' 
+            }
+            else if (dadoPlayer = dadoComputer){
+                messaggio2.textContent = 'pareggio!' 
+            }
             break;
         } 
-
         else {
             messaggio1.textContent = 'login errato!'
         }
